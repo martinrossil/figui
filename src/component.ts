719d974d4c2af6f202ef27aaecfda9b1cd7724df
martinrossil-acies <mro@acies.dk>
2025-04-05 21:21:36 +0200

    feat: add display inline-block

--- src/component.ts
+++ src/component.ts
@@ -1,6 +1,7 @@
 export default class Component extends HTMLElement {
 	public constructor() {
 		super();
+		this.style.display = 'inline-block';
 	}
 }
 customElements.define('figui-component', Component);
