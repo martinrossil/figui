f7c2e669a5a97ff822a842f03a308c40a3aebfc3
martinrossil-acies <mro@acies.dk>
2025-04-05 21:24:23 +0200

    test: create vitest.config.ts

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	test: {
+		browser: {
+			enabled: true,
+			headless: true,
+			instances: [{ browser: 'chromium' }],
+			provider: 'playwright',
+			screenshotFailures: false,
+			viewport: {
+				height: 1000,
+				width: 1000,
+			},
+		},
+		coverage: {
+			reporter: ['lcovonly'],
+		},
+	},
+});
